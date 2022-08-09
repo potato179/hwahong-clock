70e4e4e71e3f55dacce7b94e898b4bba335523fa
홍은표 <44293278+potato179@users.noreply.github.com>
2022-08-09 20:52:49 +0900

    Update get.js

--- get.js
+++ get.js
@@ -21,7 +21,7 @@ function write_calander(req, res, next){
     var events_arr = req.query.events;
     var broadcast_arr = req.query.broadcast;
     for(var i = 0; i < 7; i++){
-        con.query(`update calander set broadcast = "${broadcast_arr[i]}", events = "${events_arr[i]}"; where day = "${i}"`, function(err, result){
+        con.query(`update calander set broadcast = "${broadcast_arr[i]}", events = "${events_arr[i]}"; where day = "${i}";`, function(err, result){
             if(err) throw err;
             console.log(result);
             res.send({
